6223dac43916d046db127c6d97fd816eff7413dc
ycuervob <64037012+ycuervob@users.noreply.github.com>
2024-05-05 15:51:55 -0500

    Todo sobre maven

--- src/main/java/jena/RDFSplitter.java
+++ src/main/java/jena/RDFSplitter.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.hadoop.rdf.types.NodeWritable;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
